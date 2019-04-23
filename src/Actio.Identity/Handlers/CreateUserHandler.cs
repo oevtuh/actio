989b9ec3ada65655da4da3bbb98bd63ce0a2022c
oevtukh <oevtuh@gmail.com>
2019-04-23 22:22:15 +0300

    Added CreateUserHandler.

---
+++ src/Actio.Identity/Handlers/CreateUserHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actio.Common.Commands;
+using Actio.Common.Events;
+using RawRabbit;
+
+namespace Actio.identity.Handlers
+{
+    public class CreateUserHandler : ICommandHandler<CreateUser>
+    {
+        private readonly IBusClient _busClient;
+        public CreateUserHandler(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        public async Task HandleAsync(CreateUser command)
+        {
+            Console.WriteLine($"Creating Activity: {command.Name}");
+            await _busClient.PublishAsync(new UserCreated(command.Email, command.Name));
+        }
+    }
+}
