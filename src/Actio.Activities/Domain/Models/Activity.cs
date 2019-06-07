9f8db823c8751687d600620fc1db8367a4f1fd14
oevtukh <oevtuh@gmail.com>
2019-06-07 16:08:55 +0300

    Added Activity and Category models.

---
+++ src/Actio.Activities/Domain/Models/Activity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Activities.Domain.Models
+{
+    public class Activity
+    {
+        public Activity(Guid id, Category category, Guid userId, string name, string description, DateTime createdAt)
+        {
+            Id = id;
+            Category = category.Name;
+            UserId = userId;
+            Name = name;
+            Description = description;
+            CreatedAt = createdAt;
+        }
+        protected Activity()
+        {
+
+        }
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Category { get; protected set; }
+        public string Description { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+    }
+}
---
+++ src/Actio.Activities/Domain/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Activities.Domain.Models
+{
+    public class Category
+    {
+        public Category(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name.ToLowerInvariant();
+        }
+        protected Category()
+        {
+            
+        }
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+    }
+}
